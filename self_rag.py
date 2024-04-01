ad7c8debdbb1a25b93d8245d271d2e7ef580edfb
Thang Viet Ta <110162213+thangvta@users.noreply.github.com>
2024-04-01 15:32:39 +0700

    update selfrag

--- self_rag.py
+++ self_rag.py
@@ -1,11 +1,14 @@
 from modular_rag import ModularRAG
 from llama_index.packs.self_rag.base import SelfRAGQueryEngine
 from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 
 class SelfRAG(ModularRAG):
     def __init__(self):
         super().__init__()
+        self.Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
 
     def processing(self, file_path):
         print('Loading documents')
@@ -27,6 +30,13 @@ class SelfRAG(ModularRAG):
     def create_query_engine(self, model_path):
         from pathlib import Path
         download_dir = None
-        model_path = str(Path(download_dir))
+        model_path = str(Path(download_dir)) / "selfrag_llama2_7b.q4_k_m.gguf"
         self.query_engine = SelfRAGQueryEngine(model_path, self.retriever, verbose=True)
+
+    def download_model(self):
+        import subprocess
+        download_dir = "/model"
+        subprocess.run("pip install -q huggingface-hub", shell=True, check=True)
+        subprocess.run(f"pip huggingface-cli download m4r1/selfrag_llama2_7b-GGUF selfrag_llama2_7b.q4_k_m.gguf --local-dir {download_dir} --local-dir-use-symlinks False
+ -q huggingface-hub", shell=True, check=True)
 
